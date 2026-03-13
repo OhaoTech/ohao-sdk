c78a7facacfb7fc8ccad827f1cb839bb7341ad0c
Qervas <djmax96945147@outlook.com>
2026-03-13 18:38:21 +0100

    Fix version assertion in test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -8,7 +8,7 @@ def test_version():
     runner = CliRunner()
     result = runner.invoke(main, ["--version"])
     assert result.exit_code == 0
-    assert "0.1.0" in result.output
+    assert "0.2.0" in result.output
 
 
 def test_mogen3d_help():
